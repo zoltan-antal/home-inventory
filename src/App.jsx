fe430745f5ffa02cb5a4e128f7e8eaeeb1f1aa62
zolantal <zoltan_antal@outlook.com>
2024-02-29 10:31:56 +0000

    Setup barcode scanning using react-zxing library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div>Hello</div>;
-}
+import BarcodeScanner from './components/BarcodeScanner';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Barcode Scanner</h1>
+      <BarcodeScanner />
+    </div>
+  );
+};
 
 export default App;
